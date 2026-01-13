293970083fc18515c8622c935dc19d0009516df6
olegrewko <olegrewko@gmail.com>
2026-01-13 08:01:22 +0300

    product-hero__slider

--- src/js/components/product-slider.js
+++ src/js/components/product-slider.js
@@ -4,18 +4,18 @@ import vars from "../_vars.js";
 
 Swiper.use([Pagination]);
 
-// if (vars.productSlider) {
-//  const swiper = new Swiper(vars.productSlider, {
-//   slidesPerView: 1,
-//   spaceBetween: 20,
-//   loop: true,
-//     pagination: {
-//       el: ".product-hero__pag",
-//       clickable: true,
+if (vars.productSlider) {
+ const swiper = new Swiper(vars.productSlider, {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+    pagination: {
+      el: ".product-hero__pag",
+      clickable: true,
 
-//   }
-//  });
-// }
+  }
+ });
+}
 
 // products-slider.js
 document.addEventListener('DOMContentLoaded', function() {
